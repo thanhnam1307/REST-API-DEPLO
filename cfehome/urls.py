d872961f82ce02d401d95af5c36c246adbd93c58
nam nguyen thanh <bffkudo1412@gmail.com>
2023-08-19 00:33:35 +0700

    ok

--- cfehome/urls.py
+++ cfehome/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
@@ -25,5 +26,5 @@ urlpatterns = [
     path('api/products/', include('products.urls')),
     path('api/v2/', include('cfehome.routers'))
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += staticfiles_urlpatterns()
-# localhost:8000/api/
